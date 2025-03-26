966e7d96142ffaf117c40656d6dfb3a2ebe6ea11
Silvio Giebl <gieblsilvio@outlook.com>
2025-03-26 19:05:39 +0100

    Setup

---
+++ README.md
@@ -0,0 +1,21 @@
+# Gradle Maven Central Publishing Plugin
+
+[![Gradle Plugin Portal](https://img.shields.io/gradle-plugin-portal/v/io.github.sgtsilvio.gradle.maven-central-publishing?color=brightgreen&style=for-the-badge)](https://plugins.gradle.org/plugin/io.github.sgtsilvio.gradle.maven-central-publishing)
+[![GitHub](https://img.shields.io/github/license/sgtsilvio/gradle-maven-central-publishing?color=brightgreen&style=for-the-badge)](LICENSE)
+[![GitHub Workflow Status (with branch)](https://img.shields.io/github/actions/workflow/status/sgtsilvio/gradle-maven-central-publishing/check.yml?branch=main&style=for-the-badge)](https://github.com/SgtSilvio/gradle-maven-central-publishing/actions/workflows/check.yml?query=branch%3Amain)
+
+Gradle plugin to ease publishing to Maven Central.
+
+## How to Use
+
+```kotlin
+plugins {
+    id("io.github.sgtsilvio.gradle.maven-central-publishing") version "0.1.0"
+}
+```
+
+## Requirements
+
+- Gradle X.Y or higher
+
+## Configuration
---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    `kotlin-dsl`
+    signing
+    alias(libs.plugins.pluginPublish)
+    alias(libs.plugins.defaults)
+    alias(libs.plugins.metadata)
+}
+
+group = "io.github.sgtsilvio.gradle"
+
+metadata {
+    readableName = "Gradle Maven Central Publishing Plugin"
+    description = "Gradle plugin to ease publishing to Maven Central"
+    license {
+        apache2()
+    }
+    developers {
+        register("SgtSilvio") {
+            fullName = "Silvio Giebl"
+        }
+    }
+    github {
+        org = "SgtSilvio"
+        issues()
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(8)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+gradlePlugin {
+    plugins {
+        create("mavenCentralPublishing") {
+            id = "$group.maven-central-publishing"
+            implementationClass = "$group.mavencentral.publishing.MavenCentralPublishingPlugin"
+            tags = listOf("maven-central", "publishing")
+        }
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+}

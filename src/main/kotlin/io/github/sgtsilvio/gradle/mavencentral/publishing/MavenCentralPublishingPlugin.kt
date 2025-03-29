c719fff52390604842cc6584476101f2d44b25a0
Silvio Giebl <gieblsilvio@outlook.com>
2025-03-29 21:28:41 +0100

    Improve MavenCentralPublishingPlugin

--- src/main/kotlin/io/github/sgtsilvio/gradle/mavencentral/publishing/MavenCentralPublishingPlugin.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/mavencentral/publishing/MavenCentralPublishingPlugin.kt
@@ -3,10 +3,13 @@ package io.github.sgtsilvio.gradle.mavencentral.publishing
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.credentials.PasswordCredentials
+import org.gradle.api.file.Directory
+import org.gradle.api.provider.Provider
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.api.tasks.Delete
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.bundling.Zip
 import org.gradle.kotlin.dsl.*
 import org.gradle.plugins.signing.SigningPlugin
@@ -27,49 +30,76 @@ class MavenCentralPublishingPlugin : Plugin<Project> {
             name = STAGING_REPOSITORY_NAME
             url = project.uri(stagingRepositoryDirectory)
         }
-        val cleanTask = project.tasks.register<Delete>("clean${STAGING_REPOSITORY_NAME.capitalize()}Repository") {
-            group = TASK_GROUP_NAME
-            description = "Cleans the local '$STAGING_REPOSITORY_NAME' repository directory."
-            delete(stagingRepositoryDirectory)
-        }
-        val bundleTask = project.tasks.register<Zip>("mavenCentralBundle") {
-            group = TASK_GROUP_NAME
-            description = "Bundles the local '$STAGING_REPOSITORY_NAME' repository content."
-            from(stagingRepositoryDirectory)
-            destinationDirectory.set(outputDirectory)
-            archiveFileName.set("bundle.zip")
-        }
-        val uploadBundleTask = project.tasks.register<MavenCentralUploadTask>("uploadMavenCentralBundle") {
-            group = TASK_GROUP_NAME
-            description = "Uploads, validates and publishes the '${bundleTask.name}' to Maven Central."
-            bundleFile.set(bundleTask.flatMap { it.archiveFile })
-            credentials.set(project.providers.credentials(PasswordCredentials::class, "mavenCentral"))
-            deploymentName.set(project.provider { "${project.group}:${project.name}:${project.version}" })
-            deploymentIdFile.set(outputDirectory.map { it.file("deployment-id.txt") })
-        }
+        val cleanStagingRepositoryTask = registerCleanStagingRepositoryTask(project, stagingRepositoryDirectory)
+        val bundleTask = registerBundleTask(project, stagingRepositoryDirectory, outputDirectory)
+        val uploadBundleTask = registerUploadBundleTask(project, bundleTask, outputDirectory)
         publishingExtension.publications.withType<MavenPublication> {
             val publicationName = name
-            val publishTask =
-                project.tasks.named("publish${publicationName.capitalize()}PublicationTo${STAGING_REPOSITORY_NAME.capitalize()}Repository")
-            publishTask {
-                dependsOn(cleanTask)
-            }
-            bundleTask {
-                mustRunAfter(publishTask)
-            }
-            project.tasks.register("publish${publicationName.capitalize()}PublicationToMavenCentral") {
-                group = TASK_GROUP_NAME
-                description = "Publishes Maven publication '$publicationName' to Maven Central."
-                dependsOn(publishTask)
-                dependsOn(uploadBundleTask)
-            }
+            val publishToStagingRepositoryTask = getPublishToStagingRepositoryTask(project, publicationName)
+            publishToStagingRepositoryTask { dependsOn(cleanStagingRepositoryTask) }
+            bundleTask { mustRunAfter(publishToStagingRepositoryTask) }
+            registerPublishTask(project, publicationName, publishToStagingRepositoryTask, uploadBundleTask)
         }
-        project.tasks.register("publishToMavenCentral") {
-            group = TASK_GROUP_NAME
-            description = "Publishes all Maven publications produced by this project to Maven Central."
-            dependsOn(project.tasks.named("publishAllPublicationsTo${STAGING_REPOSITORY_NAME.capitalize()}Repository"))
-            dependsOn(uploadBundleTask)
-        }
+        registerPublishAllTask(project, uploadBundleTask)
+    }
+
+    private fun registerCleanStagingRepositoryTask(
+        project: Project,
+        stagingRepositoryDirectory: Provider<Directory>,
+    ) = project.tasks.register<Delete>("clean${STAGING_REPOSITORY_NAME.capitalize()}Repository") {
+        group = TASK_GROUP_NAME
+        description = "Cleans the local '$STAGING_REPOSITORY_NAME' repository directory."
+        delete(stagingRepositoryDirectory)
+    }
+
+    private fun registerBundleTask(
+        project: Project,
+        stagingRepositoryDirectory: Provider<Directory>,
+        outputDirectory: Provider<Directory>,
+    ) = project.tasks.register<Zip>("mavenCentralBundle") {
+        group = TASK_GROUP_NAME
+        description = "Bundles the local '$STAGING_REPOSITORY_NAME' repository content."
+        from(stagingRepositoryDirectory)
+        destinationDirectory.set(outputDirectory)
+        archiveFileName.set("bundle.zip")
+    }
+
+    private fun registerUploadBundleTask(
+        project: Project,
+        bundleTask: TaskProvider<Zip>,
+        outputDirectory: Provider<Directory>,
+    ) = project.tasks.register<MavenCentralUploadTask>("uploadMavenCentralBundle") {
+        group = TASK_GROUP_NAME
+        description = "Uploads, validates and publishes the '${bundleTask.name}' to Maven Central."
+        bundleFile.set(bundleTask.flatMap { it.archiveFile })
+        credentials.set(project.providers.credentials(PasswordCredentials::class, "mavenCentral"))
+        deploymentName.set(project.provider { "${project.group}:${project.name}:${project.version}" })
+        deploymentIdFile.set(outputDirectory.map { it.file("deployment-id.txt") })
+    }
+
+    private fun getPublishToStagingRepositoryTask(project: Project, publicationName: String) =
+        project.tasks.named("publish${publicationName.capitalize()}PublicationTo${STAGING_REPOSITORY_NAME.capitalize()}Repository")
+
+    private fun registerPublishTask(
+        project: Project,
+        publicationName: String,
+        publishToStagingRepositoryTask: TaskProvider<*>,
+        uploadBundleTask: TaskProvider<MavenCentralUploadTask>,
+    ) = project.tasks.register("publish${publicationName.capitalize()}PublicationToMavenCentral") {
+        group = TASK_GROUP_NAME
+        description = "Publishes Maven publication '$publicationName' to Maven Central."
+        dependsOn(publishToStagingRepositoryTask)
+        dependsOn(uploadBundleTask)
+    }
+
+    private fun registerPublishAllTask(
+        project: Project,
+        uploadBundleTask: TaskProvider<MavenCentralUploadTask>,
+    ) = project.tasks.register("publishToMavenCentral") {
+        group = TASK_GROUP_NAME
+        description = "Publishes all Maven publications produced by this project to Maven Central."
+        dependsOn(project.tasks.named("publishAllPublicationsTo${STAGING_REPOSITORY_NAME.capitalize()}Repository"))
+        dependsOn(uploadBundleTask)
     }
 }
 

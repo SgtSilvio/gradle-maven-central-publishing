359a60ab413556bfb6e89bb80e77cbe8380d065e
Silvio Giebl <gieblsilvio@outlook.com>
2025-03-29 20:12:28 +0100

    Use group:name:version as deployment name

--- src/main/kotlin/io/github/sgtsilvio/gradle/mavencentral/publishing/MavenCentralPublishingPlugin.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/mavencentral/publishing/MavenCentralPublishingPlugin.kt
@@ -44,7 +44,7 @@ class MavenCentralPublishingPlugin : Plugin<Project> {
             description = "Uploads, validates and publishes the '${bundleTask.name}' to Maven Central."
             bundleFile.set(bundleTask.flatMap { it.archiveFile })
             credentials.set(project.providers.credentials(PasswordCredentials::class, "mavenCentral"))
-            deploymentName.set("testname") // TODO
+            deploymentName.set(project.provider { "${project.group}:${project.name}:${project.version}" })
             deploymentIdFile.set(outputDirectory.map { it.file("deployment-id.txt") })
         }
         publishingExtension.publications.withType<MavenPublication> {

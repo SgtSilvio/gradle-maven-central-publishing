900e4937049426700f0f8e067b7d3c658a1fb342
Silvio Giebl <gieblsilvio@outlook.com>
2025-03-30 18:25:18 +0200

    Cleanup MavenCentralUploadTask

--- src/main/kotlin/io/github/sgtsilvio/gradle/mavencentral/publishing/MavenCentralUploadTask.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/mavencentral/publishing/MavenCentralUploadTask.kt
@@ -47,54 +47,47 @@ abstract class MavenCentralUploadTask : DefaultTask() {
 
         val publisherApi = MavenCentralPublisherApi(baseUrl, credentials.username!!, credentials.password!!)
         val deploymentId = publisherApi.upload(bundleFile, deploymentName, isPublish)
-        logger.quiet("maven central deployment id: $deploymentId")
         deploymentIdFile.writeText(deploymentId)
-        publisherApi.waitForState(
-            deploymentId,
-            when {
-                !isPublish -> setOf("VALIDATED")
-                !waitUntilFullyPublished -> setOf("PUBLISHING", "PUBLISHED")
-                else -> setOf("PUBLISHED")
-            },
-            when {
-                !isPublish -> setOf("PUBLISHING", "PUBLISHED", "FAILED")
-                else -> setOf("FAILED")
-            },
-        )
-    }
 
-    private fun MavenCentralPublisherApi.waitForState(
-        deploymentId: String,
-        expectedStates: Set<String>,
-        unexpectedStates: Set<String>,
-    ) {
+        val message = "maven central deployment id: $deploymentId"
+        logger.quiet(message)
+        val expectedStates = when {
+            !isPublish -> setOf("VALIDATED")
+            !waitUntilFullyPublished -> setOf("PUBLISHING", "PUBLISHED")
+            else -> setOf("PUBLISHED")
+        }
+        val unexpectedStates = when {
+            !isPublish -> setOf("PUBLISHING", "PUBLISHED", "FAILED")
+            else -> setOf("FAILED")
+        }
         var previousState: String? = null
         var errorCount = 0
         while (true) {
             val status = try {
-                getStatus(deploymentId)
+                publisherApi.getStatus(deploymentId)
             } catch (e: IllegalStateException) {
                 if (++errorCount > 3) {
                     throw e
                 }
-                logger.lifecycle("maven central deployment id: $deploymentId, state request failed for $errorCount times, treating as temporary error: ${e.message}")
-                continue // TODO delay
+                logger.lifecycle("$message, state request failed for $errorCount times, treating as temporary error: ${e.message}")
+                TimeUnit.SECONDS.sleep(1)
+                continue
             }
             errorCount = 0
             val state = status.getString("deploymentState")
-            val message = "maven central deployment id: $deploymentId, state: $state"
+            val stateMessage = "$message, state: $state"
             if (state in expectedStates) {
-                logger.lifecycle(message)
+                logger.lifecycle(stateMessage)
                 break
             }
             if (state in unexpectedStates) {
-                val failedMessage = "$message (expected ${expectedStates.joinToString(" or ")})"
+                val failedMessage = "$stateMessage (expected ${expectedStates.joinToString(" or ")})"
                 logger.lifecycle(failedMessage)
                 val errors = status.optJSONObject("errors")?.toString(2)
                 val errorMessage = if (errors == null) failedMessage else "$failedMessage, errors:\n$errors"
                 error(errorMessage)
             }
-            val waitingMessage = "$message (waiting for ${expectedStates.joinToString(" or ")})"
+            val waitingMessage = "$stateMessage (waiting for ${expectedStates.joinToString(" or ")})"
             if (state != previousState) {
                 logger.lifecycle(waitingMessage)
             } else {
